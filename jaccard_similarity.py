5b8e7adb0b9ac67498955e760d5bcb6f427e647b
Ani Ghosh <ganiruddhag@gmail.com>
2023-06-01 09:19:24 -0500

    added intersection and union

--- jaccard_similarity.py
+++ jaccard_similarity.py
@@ -1,3 +1,6 @@
-#This function will return both Jaccard distance and similarity
-def jaccard_compute():
+#This function will take two sets and return both Jaccard distance and similarity. Make sure that the two inputs are sets and not lists or other types of data structures
+def jaccard_compute(set1, set2):
+    intersection = set1.intersection(set2)
+    union = set1.union(set2)
+    intersection_count = count(intersection)
     return()
